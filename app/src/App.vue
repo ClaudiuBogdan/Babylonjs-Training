7b66662e99216022f0a985edd8dfcf15ed24b270
Claudiu C. Bogdan <bogdan.claudiu93@gmail.com>
2019-10-12 21:28:30 +0300

    Add and remove mesh interactions.

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,7 +8,17 @@
     import {Component, Vue} from 'vue-property-decorator';
     import {Scene} from '@babylonjs/core/scene';
     import {Engine} from '@babylonjs/core/Engines/engine';
-    import {FreeCamera, HemisphericLight, Light, Mesh, MeshBuilder, Vector3} from "@babylonjs/core";
+    import {
+        ActionManager,
+        Color3, ExecuteCodeAction,
+        FreeCamera,
+        HemisphericLight,
+        Light,
+        Mesh,
+        MeshBuilder,
+        StandardMaterial,
+        Vector3
+    } from "@babylonjs/core";
 
     @Component({})
 
@@ -42,6 +52,7 @@
         private _scene: Scene;
         private _camera: FreeCamera;
         private _light: Light;
+        private _sphere: Mesh;
 
         constructor(canvasElement: string) {
             // Create canvas and engine.
@@ -56,6 +67,10 @@
 
             // Create a basic light, aiming 0,1,0 - meaning, to the sky.
             this._light = new HemisphericLight('light1', new Vector3(1, 1, 0), this._scene);
+
+            // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
+            this._sphere = MeshBuilder.CreateSphere('sphere1',
+                {segments: 16, diameter: 2}, this._scene);
         }
 
         createScene(): void {
@@ -65,16 +80,110 @@
             // Attach the camera to the canvas.
             this._camera.attachControl(this._canvas, false);
 
-            // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
-            let sphere = MeshBuilder.CreateSphere('sphere1',
-                {segments: 16, diameter: 2}, this._scene);
-
             // Move the sphere upward 1/2 of its height.
-            sphere.position.y = 1;
+            this._sphere.position.y = 1;
 
             // Create a built-in "ground" shape.
             let ground = MeshBuilder.CreateGround('ground1',
                 {width: 6, height: 6, subdivisions: 2}, this._scene);
+
+            this.setCollisionScene();
+        }
+
+        setCollisionScene(): void{
+            const scene = this._scene;
+            const sphere = this._sphere;
+            scene.collisionsEnabled = true;
+            const redMaterial = new StandardMaterial("redMaterial", scene);
+            redMaterial.diffuseColor = new Color3(1, 0, 0);
+
+            const blueMaterial = new StandardMaterial("blueMaterial", scene);
+            blueMaterial.diffuseColor = new Color3(0, 0, 1);
+
+            const box = Mesh.CreateBox("box", 1, scene);
+            box.position = new Vector3(-2, 0.5, 0);
+            box.material = redMaterial;
+
+            const box2 = Mesh.CreateBox("box2", 0.5, scene);
+            box2.position = new Vector3(2, 0.25, 0);
+            box2.material = blueMaterial;
+
+            /* Movement */
+            const keys = {
+                move: true,
+                blocked: false,
+                direction: new Vector3(0, 0, 0),
+                speed: 0.01
+            };
+
+            box.actionManager = new ActionManager(scene);
+            const collisionObjectList = [box2, sphere];
+            const registeredActionList = this.registerCollisionDetection(box, collisionObjectList, keys);
+
+            setTimeout(()=>{
+                this.unregisterCollisionDetection(box, registeredActionList, keys)
+                console.log("Remove interactions");
+            }, 5000);
+
+
+            const ground = Mesh.CreateGround("ground", 10, 10, 2, scene);
+            ground.checkCollisions = true;
+
+            window.addEventListener("keydown", handleKeyDown, false);
+            window.addEventListener("keyup",   handleKeyUp,   false);
+
+            function handleKeyDown(evt: KeyboardEvent) {
+                if (evt.key=='a') {
+                    keys.direction.x = -10;
+                }
+                if (evt.key=='d') {
+                    keys.direction.x = 10;
+                }
+                if (evt.key=='w') {
+                    keys.direction.z = 10;
+                }
+                if (evt.key=='s') {
+                    keys.direction.z = -10;
+                }
+            }
+
+            function handleKeyUp(evt: KeyboardEvent) {
+                keys.direction = Vector3.Zero();
+            }
+
+            scene.registerBeforeRender(function() {
+                if (keys.move) {
+                    box.position.addInPlace(keys.direction.scale(keys.speed));
+                }
+            });
+        }
+
+        registerCollisionDetection(mainObject: Mesh, collisionObjectList: Array<Mesh>, keys: any): Array<ExecuteCodeAction>{
+            const registeredActions = [] as Array<ExecuteCodeAction>;
+            collisionObjectList.forEach(object => {
+                //@ts-ignore
+                registeredActions.push(mainObject.actionManager.registerAction(new ExecuteCodeAction({
+                    trigger: ActionManager.OnIntersectionEnterTrigger,
+                    parameter: object
+                }, function () {
+                    mainObject.position.subtractInPlace(keys.direction.scale(keys.speed));
+                    keys.move = false;
+                })));
+                //@ts-ignore
+                registeredActions.push(mainObject.actionManager.registerAction(new ExecuteCodeAction({
+                    trigger: ActionManager.OnIntersectionExitTrigger,
+                    parameter: object
+                },function () {keys.move = true})));
+            });
+            return registeredActions;
+        }
+
+        unregisterCollisionDetection(mainObject: Mesh, registeredActionList: Array<ExecuteCodeAction>, keys: any){
+            registeredActionList.forEach(action => {
+                //@ts-ignore
+                mainObject.actionManager.unregisterAction(action);
+            });
+            keys.move = true
         }
 
         doRender(): void {
